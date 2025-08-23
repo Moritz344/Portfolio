2ebcd9b5b7824363d5c4a7e04a70562b077e44a8
Moritz344 <moritzmaier34@gmail.com>
2025-08-23 22:15:04 +0200

    feat: added imgages and components

---
+++ personal-site/src/app/project/project.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-project',
+  imports: [],
+  templateUrl: './project.component.html',
+  styleUrl: './project.component.css'
+})
+export class ProjectComponent {
+  @Input() projectName: string = "";
+  @Input() projectDesc: string = "";
+  @Input() projectLink: string = "";
+  @Input() projectImage: string = "";
+  @Input() projectImageWidth: number = 0;
+  @Input() projectImageHeight: number = 0;
+
+  imageLoaded = false;
+
+  constructor() {}
+
+  handleClick() {
+
+    console.log("klick");
+
+  }
+
+}
