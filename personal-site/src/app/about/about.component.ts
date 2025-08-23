0566ddda71c524a475d6ae890ebfa1b7ad4c9f14
Moritz344 <moritzmaier34@gmail.com>
2025-08-23 22:13:58 +0200

    feat: home page/about page

---
+++ personal-site/src/app/about/about.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+// TODO: zeitleiste von meinem leben Schule Praktikum etc
+
+@Component({
+  selector: 'app-about',
+  imports: [RouterModule],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+
+}
