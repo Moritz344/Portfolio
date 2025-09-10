665a27abeb406f2ab92ad14a8584b1578966ed90
moritz <moritzmaier353@gmail.com>
2025-09-10 14:45:29 +0200

    added correct files with changes: blog box that shows more blog entries/changed color on tags

--- personal-site/src/app/blog-element/blog-element.component.ts
+++ personal-site/src/app/blog-element/blog-element.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { BlogSettingsService } from '../services/blog-settings.service';
 import { Blog } from '../models/blog.models';
 import { TopbarComponent } from '../topbar/topbar.component';
+import { RouterModule } from '@angular/router';
+
+// TODO: liste mit blog einträgen links zu denen man switchen kann
 
 @Component({
   selector: 'app-blog-element',
-  imports: [TopbarComponent],
+  imports: [TopbarComponent,RouterModule],
   templateUrl: './blog-element.component.html',
   styleUrl: './blog-element.component.css'
 })
@@ -16,18 +19,32 @@ export class BlogElementComponent {
   blogDate = "";
   blogText = "";
 
+  blogItems: Blog[] = [];
+  blogResults = true;
+
   constructor(private blogSettingsService: BlogSettingsService) {
 
-    this.blogData = this.blogSettingsService.getCurrentSelectedBlog();
-    this.blogName = this.blogData[0].name;
-    this.blogDate = this.blogData[0].date;
-    this.blogText= this.blogData[0].text;
-
-
+	this.initBlog();
 
 
 
   }
 
+
+  initBlog() {
+    this.blogData = this.blogSettingsService.getCurrentSelectedBlog();
+    this.blogName = this.blogData[0].name;
+    this.blogDate = this.blogData[0].date;
+    this.blogText= this.blogData[0].text;
+
+    this.blogItems = this.blogSettingsService.getBlogItems();
+    console.log(this.blogItems);
+
+  }
+
+  saveBlog(name: string,date: string,text: string) {
+    this.blogSettingsService.setCurrentSelected(date,name,text);
+    this.initBlog();
+  }
 
 }
