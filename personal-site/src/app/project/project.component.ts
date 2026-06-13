171c092581c83b03fd64e784e0fa637ad5ecffa7
Moritz344 <moritzmaier34@gmail.com>
2026-06-13 22:29:00 +0200

    click on img see picture-list/use width and height/

--- personal-site/src/app/project/project.component.ts
+++ personal-site/src/app/project/project.component.ts
@@ -1,29 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input,OnInit,ViewChild,ElementRef,signal, EventEmitter,Output } from '@angular/core';
 import { BadgeComponent } from '../badge/badge.component';
 import { CommonModule } from '@angular/common';
 import { Project } from '../models/project.models';
 import { TiltDirective } from '../directives/tilt.directive';
+import { PictureListComponent } from './picture-list/picture-list.component';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [BadgeComponent, CommonModule,TiltDirective],
+  imports: [BadgeComponent, CommonModule,TiltDirective,PictureListComponent],
   templateUrl: './project.component.html',
   styleUrl: './project.component.css'
 })
-export class ProjectComponent {
+export class ProjectComponent implements OnInit {
+  @ViewChild("projectImg") projectImg!: ElementRef;
   @Input() data: Project[] = [];
+  @Output() showPictures = new EventEmitter<string[]>();
 
-  imageLoaded = false;
+  public imageLoaded = false;
 
   openLink(link: string) {
     window.open(link, '_blank');
   }
 
   constructor() {
-    console.log(this.data);
+  }
+
+  onOpenPictureList(images: string[]) {
+    this.showPictures.emit(images);
   }
 
 
+  ngOnInit(): void {
+  }
 
 }
