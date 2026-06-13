7060865ab762523f3835e0e37766f452d2eae06f
Moritz344 <moritzmaier34@gmail.com>
2026-06-13 22:28:14 +0200

    remove todo/add images to projects/import project list component

--- personal-site/src/app/front-page/front-page.component.ts
+++ personal-site/src/app/front-page/front-page.component.ts
@@ -8,12 +8,11 @@ import { BadgeComponent } from "../badge/badge.component";
 import { Project } from "../models/project.models";
 import { BottomComponent } from '../bottom/bottom.component';
 import { TimelineComponent } from '../timeline/timeline.component';
-import {NgOptimizedImage} from '@angular/common';
+import { NgOptimizedImage } from '@angular/common';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { TiltDirective } from '../directives/tilt.directive';
 import { WorkExperienceComponent } from "../work-experience/work-experience.component";
-
-// TODO: show work experience
+import { PictureListComponent } from "../project/picture-list/picture-list.component";
 
 @Component({
   selector: "app-front-page",
@@ -22,6 +21,7 @@ import { WorkExperienceComponent } from "../work-experience/work-experience.comp
     WorkExperienceComponent,
     TimelineComponent,
     ProjectComponent,
+    PictureListComponent,
     AboutComponent,
     NgOptimizedImage,
     SkillsComponent,
@@ -53,67 +53,49 @@ export class FrontPageComponent implements AfterViewInit {
   public isShownWork = signal(false);
   public reveal: number = 0;
 
+  public showProjectImages = signal(false);
+  public moreProjectImages = signal<string[]>([]);
+
   projects: Project[] = [
     {
       name: "Anime Web App",
       lang: "Typescript",
       link: "https://github.com/Moritz344/GruvAnime",
-      img: "gruvanime.png",
+      images: ["gruvanime_new.png","gruvanime_1.png",  "gruvanime_2.png","gruvanime_3.png"],
       desc: "Eine Website zum Durchsuchen von Mangas und Animes. Es gibt die Möglichkeit, beliebte, kommende und gerade veröffentlichte Animes und Mangas zu durchsuchen. Beim Anklicken eines Animes oder Mangas werden weitere Informationen angezeigt, wie Genres, Anzahl der Episoden, Status, Beschreibung etc. Außerdem kann über die Kalenderansicht gesehen werden, welche Animes an welchen Tagen und um wie viel Uhr erscheinen.",
       side: "right",
+      width: "800px",
+      height: "500px",
     },
-
     {
-      name: "Texteditor",
-      lang: "Python",
-      link: "https://github.com/Moritz344/Texteditor",
-      img: "texteditor.png",
-      desc: "Ein einfach gehaltener Texteditor für Notizen oder zum Schreiben von Python Code. Er bietet Syntax Higlighting für Python Datein, einen Markdown Editor und mehr.",
+      name: "Minecraft Bot",
+      lang: "Typescript",
+      link: "https://github.com/Moritz344/bob-the-fisherman",
+      images: ["bot_1.png","bot_2.png","bot_3.png","bot_4.png"],
+      desc: "Ein Bot für das Spiel Minecraft der automatisch fisht. Für den Bot gibt es sowohl eine Desktop App als auch ein Befehlseingabetool. Es können Befehle über die App eingegeben werden,die gesammelten Gegenständen in einer Tabelle eingesehen werden und Bot Einstellungen angepasst werden.",
       side: "left",
+      width: "800px",
+      height: "500px",
     },
     {
       name: "Spiel Info Website",
       lang: "Typescript",
       link: "https://github.com/Moritz344/raid-log",
-      img: "arc_raiders.jpg",
+      images: ["arc_raiders.jpg","arc_2.png","arc_3.png","arc_4.png","arc_5.png"],
       desc: "Eine Info Website über das Spiel Arc Raiders. Es gibt die Möglichkeit, nach Gegenständen aus dem Spiel, nach Quests, nach Gegnern (Arcs) und nach Händlern zu suchen.",
       side: "right",
+      width: "800px",
+      height: "500px",
     },
     {
       name: "Twitch Client",
       lang: "Typescript",
       link: "https://github.com/Moritz344/lurker",
-      img: "lurker_new.png",
+      images: ["twitch_client.png","twitch_client_2.png"],
       desc: "Ein Chat Client für twitch.tv. Die App ermöglicht es, den Chat eines Twitch Kanals in Echtzeit zu verfolgen. Dabei werden User Badges, individuelle Chat Farben und Twitch Emojis angezeigt. Durch das Anklicken eines Users kann dessen User Card geöffnet werden, in der weitere Informationen sowie die neuesten Nachrichten des Users,das Profilbild und die Profil Beschreibung angezeigt wird.",
       side: "left",
-    },
-  ];
-
-
-  links = [
-    {
-      name: "Github",
-      link: "https://github.com/Moritz344",
-      desc: "Mein Github.",
-      img: "",
-    },
-
-    {
-      name: "Youtube",
-      link: "https://www.youtube.com/@pennti",
-      desc: "Mein Youtube.",
-      img: "",
-      ImgWidth: 0,
-      ImgHeight: 0,
-    },
-
-    {
-      name: "X",
-      link: "https://x.com/iuseosu",
-      desc: "Mein X.",
-      img: "",
-      ImgWidth: 0,
-      ImgHeight: 0,
+      width: "700px",
+      height: "600px",
     },
   ];
 
@@ -152,5 +134,14 @@ export class FrontPageComponent implements AfterViewInit {
     observer.observe(this.projectsSection.nativeElement);
 
     setTimeout(() => { isInitial = false; });
+  }
+
+  onCloseProjectImages() {
+    this.moreProjectImages.set([]);
+    this.showProjectImages.set(false);
+  }
+  onShowProjectImages(images: string[]) {
+    this.moreProjectImages.set(images);
+    this.showProjectImages.set(true);
   }
 }
